2200b069b8fa7c9d48950540e8182b8a9c3c0437
Zachary Kearns <zach_kearns89@live.com>
2016-08-23 19:18:03 -0400

    8-23-2016

--- server.js
+++ server.js
@@ -6,52 +6,69 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:date', function (req, res) {
-  if (Date.parse(req.params.date)) {
-    var date = new Date(Date.parse(req.params.date));
-    var natural = date.toDateString().split(" ");
-    switch (natural[1]) {
-      case "Jan":
-      natural[1] = "January";
-      break;
-      case "Feb":
-      natural[1] = "February";
-      break;
-      case "Mar":
-      natural[1] = "March";
-      break;
-      case "Apr":
-      natural[1] = "April";
-      break;
-      case "Jun":
-      natural[1] = "June";
-      break;
-      case "Jul":
-      natural[1] = "July";
-      break;
-      case "Aug":
-      natural[1] = "August";
-      break;
-      case "Sep":
-      natural[1] = "September";
-      break;
-      case "Oct":
-      natural[1] = "October";
-      break;
-      case "Nov":
-      natural[1] = "November";
-      break;
-      case "Dec":
-      natural[1] = "December";
-      break;
-    }
-    natural = natural[1] + " " + natural[2] + ", " + natural[3];
-    res.send({ "unix": Date.parse(req.params.date) / 1000, "natural": natural });
+  var date;
+  if (checkIfInteger(req.params.date)) {
+    date = new Date(parseInt(req.params.date) * 1000);
+    res.send({ "unix": parseInt(req.params.date), "natural": getNaturalDate(date) });
+  } else if (Date.parse(req.params.date)) {
+    date = new Date(Date.parse(req.params.date));
+    res.send({ "unix": Date.parse(req.params.date) / 1000, "natural": getNaturalDate(date) });
   } else {
     res.send({ "unix": null, natural: null });
   }
-
 });
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
 });
+
+function getNaturalDate(date) {
+  var natural = date.toDateString().split(" ");
+  switch (natural[1]) {
+    case "Jan":
+    natural[1] = "January";
+    break;
+    case "Feb":
+    natural[1] = "February";
+    break;
+    case "Mar":
+    natural[1] = "March";
+    break;
+    case "Apr":
+    natural[1] = "April";
+    break;
+    case "Jun":
+    natural[1] = "June";
+    break;
+    case "Jul":
+    natural[1] = "July";
+    break;
+    case "Aug":
+    natural[1] = "August";
+    break;
+    case "Sep":
+    natural[1] = "September";
+    break;
+    case "Oct":
+    natural[1] = "October";
+    break;
+    case "Nov":
+    natural[1] = "November";
+    break;
+    case "Dec":
+    natural[1] = "December";
+    break;
+  }
+  return natural = natural[1] + " " + natural[2] + ", " + natural[3];
+}
+
+function checkIfInteger(string) {
+  var isInteger = true;
+  string = string.split("");
+  for (var i = 0; i < string.length; i++) {
+    if (isNaN(string[i])) {
+      isInteger = false;
+    }
+  }
+  return isInteger;
+}
